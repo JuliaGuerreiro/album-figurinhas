43841cffd549d1b6565ef3d1f26cec678e7f4b00
Diego-PS <diegopsilvavrb@gmail.com>
2022-10-11 22:43:55 -0300

    login: autenticação e token

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
-const session = require('express-session')
 
 const usersController = require('./users/UsersController')
 
--- users/UsersController.js
+++ users/UsersController.js
@@ -4,7 +4,15 @@ const router = express.Router()
 const Users = require('./Users')
 const bcrypt = require('bcryptjs')
 const fs = require('fs')
+const jwt = require('jsonwebtoken')
 
+require('dotenv').config()
+
+/*
+Vai precisar de uma rota para criar usuários, a requisição vai ser do tipo post e o BackEnd vai receber como parâmetro o nome de usuário e a senha do usuário.
+Caso o usuário seja criado com sucesso, basta retornar status:OK
+Do contrário deve ser retornado que ocorreu um erro (Deve ser especificado se esse erro ocorreu porque o nome de usuário já estava em uso
+*/
 router.post('/users/create', (req, res) => {
     var username = req.body.username
     var password = req.body.password
@@ -26,7 +34,9 @@ router.post('/users/create', (req, res) => {
     const newUser = {
         id: uuid.v4(),
         username: req.body.username,
-        password: hash
+        password: hash,
+        token: '',
+        last_pack: 0
     }
 
     Users.push(newUser)
@@ -34,6 +44,61 @@ router.post('/users/create', (req, res) => {
     fs.writeFile('./users/Users.js', 'Users = ' + JSON.stringify(Users) + ';module.exports = Users', err => { if (err) throw err; console.log('Saved!') })
 
     res.json(Users)
+})
+
+/*
+Vai precisar de uma rota para autenticação de usuários, essa rota vai ser do tipo post e ela recebe usuário e senha. 
+Caso não seja possível autenticar, ele retorna erro e caso seja possível autenticar ele retorna um token pro usuário  
+O papel desse token vai ser garantir que o usuário está logado e garantir que a requisição de um usuário é autêntica sem precisar ficar passando a senha pra todo lado.
+*/
+router.post('/users/authenticate', (req, res) => {
+    // Authenticate User
+
+    const username = req.body.username
+    const password = req.body.password
+
+    var user = Users.find(user => user.username == username)
+
+    if (user != undefined) {
+        // Validate password
+        var correct = bcrypt.compareSync(password, user.password)
+
+        if (correct) {
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+            user.token = accessToken
+            fs.writeFile('./users/Users.js', 'Users = ' + JSON.stringify(Users) + ';module.exports = Users', err => { if (err) throw err; console.log('Saved!') })
+
+            res.json({ accessToken: accessToken })
+        } else {
+            res.json({ msg: 'Incorrect password' })
+        }
+
+    } else {
+        return res.json({ msg: 'Username not registered' })
+    }
+})
+
+/*
+Vai precisar de uma rota só pra verificar se um usuário de fato está logado, 
+pra isso vai precisar de uma rota post que recebe o nome do usuário e o token,
+o BackEnd verifica no BD se o token está certo e responde Ok ou token inválido
+*/
+router.post('/users/checktoken', (req, res) => {
+
+    const username = req.body.username
+    const token = req.body.token
+
+    var user = Users.find(user => user.username == username)
+
+    if (user != undefined) {
+        if (token == user.token) {
+            res.json({ msg: 'OK' })
+        } else {
+            res.json({ msg: 'Invalid token' })
+        }
+    } else {
+        res.json({ msg: 'Invalid user' })
+    }
 })
 
 module.exports = router
